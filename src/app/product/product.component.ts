c57c76ad3e3f8fbf8d1bdd189bd3b248baf61fba
rcelik <rahmancelik@gmail.com>
2021-05-02 12:16:08 +0300

    Products datalari server dan HttpClient kullanarak alinmasi saglandi.

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { AlertifyService } from 'src/services/alertify.service';
 import { Product } from './product';
@@ -9,24 +10,19 @@ import { Product } from './product';
 })
 export class ProductComponent implements OnInit {
 
-  constructor(private alertifyService: AlertifyService) { }
+  constructor(private alertifyService: AlertifyService, private httpClient: HttpClient) { }
 
   title = 'Urun Listesi';
-  products: Product[] = [
-    { id: 1, name: 'LapTop', price: 2500, categoryId: 1, description: 'Asus Zenbook', imageUrl: 'https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bGFwdG9wfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60' },
-    { id: 2, name: 'Mouse', price: 25, categoryId: 2, description: 'A4 Tech', imageUrl: 'https://images.unsplash.com/photo-1616071358846-9f34f471815d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bW91c2VwYWR8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60' },
-    { id: 1, name: 'LapTop', price: 2500, categoryId: 1, description: 'Asus Zenbook', imageUrl: 'https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bGFwdG9wfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60' },
-    { id: 2, name: 'Mouse', price: 25, categoryId: 2, description: 'A4 Tech', imageUrl: 'https://images.unsplash.com/photo-1616071358846-9f34f471815d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bW91c2VwYWR8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60' },
-    { id: 1, name: 'LapTop', price: 2500, categoryId: 1, description: 'Asus Zenbook', imageUrl: 'https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bGFwdG9wfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60' },
-    { id: 2, name: 'Mouse', price: 25, categoryId: 2, description: 'A4 Tech', imageUrl: 'https://images.unsplash.com/photo-1616071358846-9f34f471815d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bW91c2VwYWR8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60' },
-  ];
+  products!: Product[];
   filterText: string = '';
+  path: string = 'http://localhost:3000/products';
 
   addToCart(product: Product) {
     this.alertifyService.success('Sepete eklemeye basildi. ' + product.name);
   }
 
   ngOnInit(): void {
+    this.httpClient.get<Product[]>(this.path).subscribe( data => {this.products = data} );
   }
 
 }
