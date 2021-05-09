f092075cf4d3054faf91181f71177860e1170a5b
rcelik <rahmancelik@gmail.com>
2021-05-09 13:17:32 +0300

    Ufak bir degisiklik.

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -16,12 +16,12 @@ export class CartComponent implements OnInit {
 
   ngOnInit(): void {
     console.log(`[CartComponent.ngOnInit] is called.`);
+    this.cartItems = this.cartService.getCart();
   }
 
-  getCartItems(): Array<CartItem> {
-    const cartItems = this.cartService.getCart();
-    console.log(`Item count inside cart is ${cartItems.length}`);
-    return cartItems;
+  get getCartItems(): Array<CartItem> {
+    console.log(`Item count inside cart is ${this.cartItems.length}`);
+    return this.cartItems;
   }
 
   removeItem(item: CartItem): void {
